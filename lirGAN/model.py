dac79936deb8510c116a1d6966a93e1e7c760aaa
PARKCHEOLHEE-lab <qhqhahah@naver.com>
2024-01-16 21:22:59 +0900

    Defining custom loss function

--- lirGAN/model.py
+++ lirGAN/model.py
@@ -1,6 +1,7 @@
 
 import os
 import torch
+import torch.nn as nn
 import numpy as np
 
 from typing import List
@@ -41,6 +42,44 @@ class LirDataset(Dataset, ModelConfig):
         
         return lir_dataset
 
+class LirGeometricalLoss(nn.Module):
+    def __init__(self, bce_weight, diou_weight, area_weight, feasibility_weight):
+        super().__init__()
+        
+        self.bce_weight = bce_weight 
+        self.diou_weight = diou_weight 
+        self.area_weight = area_weight 
+        self.feasibility_weight = feasibility_weight
+        
+        self.bce_loss_function = nn.BCEWithLogitsLoss()
+
+    @staticmethod
+    def compute_diou_loss(generated_lir, target_lir, diou_weight):
+        intersection = torch.logical_and(generated_lir, target_lir)
+        union = torch.logical_or(generated_lir, target_lir)
+        iou_score = torch.sum(intersection).float() / torch.sum(union).float()
+
+        center_pred = torch.tensor([torch.mean(torch.nonzero(generated_lir, as_tuple=True)[i]) for i in range(2)])
+        center_target = torch.tensor([torch.mean(torch.nonzero(target_lir, as_tuple=True)[i]) for i in range(2)])
+        distance = torch.norm(center_pred - center_target, p=2)
+
+        diagonal = torch.norm(torch.tensor(generated_lir.shape) - 1, p=2)
+        normalized_distance = (distance / diagonal) ** 2
+
+        diou = iou_score - normalized_distance
+
+        return (1 - diou) * diou_weight
+    
+    @staticmethod
+    def compute_area_loss(generated_lir, area_weight):
+        return
+    
+    @staticmethod
+    def _compute_feasibility_loss(generated_lir, input_polygon, feasibility_weight):
+        return
+    
+    def forward(self):
+        return
 
 if __name__ == "__main__":
     lir_dataset = LirDataset()
