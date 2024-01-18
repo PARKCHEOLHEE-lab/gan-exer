fba033be34241ed8fc3ddb17b71a8044666fc1b7
PARKCHEOLHEE-lab <qhqhahah@naver.com>
2024-01-18 22:12:54 +0900

    Fix invalid syntax

--- lirGAN/model.py
+++ lirGAN/model.py
@@ -149,13 +149,12 @@ class LirGeometricLoss(nn.Module):
 
 class LirGenerator(nn.Moudle):
     def __init__(self):
-        super.__init__()
+        super().__init__()
         
 
 class LirDiscriminator(nn.Module):
     def __init__(self):
-        super.__init__()
-    
+        super().__init__()
 
 
 if __name__ == "__main__":
