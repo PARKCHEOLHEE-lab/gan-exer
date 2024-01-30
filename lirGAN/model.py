d6396ea6df0b2d6d76bf90d409b1c1de390c0d6d
PARKCHEOLHEE-lab <qhqhahah@naver.com>
2024-01-30 22:04:16 +0900

    Simplify discriminator

--- lirGAN/model.py
+++ lirGAN/model.py
@@ -222,13 +222,8 @@ class LirDiscriminator(nn.Module, ModelConfig):
             nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1, bias=False),
             nn.BatchNorm2d(256),
             nn.LeakyReLU(0.2, inplace=True),
-            nn.Conv2d(256, 512, kernel_size=4, stride=2, padding=1, bias=False),
-            nn.BatchNorm2d(512),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Conv2d(512, 512, kernel_size=4, stride=2, padding=1, bias=False),
-            nn.BatchNorm2d(512),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Conv2d(512, 1, kernel_size=15, stride=15, padding=0, bias=False),
+            nn.Conv2d(256, 1, kernel_size=4, stride=1, padding=0, bias=False),
+            nn.AdaptiveAvgPool2d(1),
             nn.Sigmoid(),
         )
 
