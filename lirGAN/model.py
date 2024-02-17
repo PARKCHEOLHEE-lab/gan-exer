07ac36b76103a216d20b522cdfaf4a416b754419
PARKCHEOLHEE-lab <qhqhahah@naver.com>
2024-02-17 14:12:26 +0900

    Cleanup losses

--- lirGAN/model.py
+++ lirGAN/model.py
@@ -60,7 +60,6 @@ class LirGeometricLoss(nn.Module):
         diou_weight: float = 1.0,
         feasibility_weight: float = 1.0,
         connectivity_weight: float = 1.0,
-        rectangular_shape_weight: float = 1.0,
     ):
         super().__init__()
 
@@ -68,7 +67,6 @@ class LirGeometricLoss(nn.Module):
         self.diou_weight = diou_weight
         self.feasibility_weight = feasibility_weight
         self.connectivity_weight = connectivity_weight
-        self.rectangular_shape_weight = rectangular_shape_weight
 
         self.bce_loss_function = nn.BCEWithLogitsLoss()
 
@@ -109,7 +107,10 @@ class LirGeometricLoss(nn.Module):
 
     @staticmethod
     def compute_feasibility_loss(
-        input_polygon: torch.Tensor, generated_lir: torch.Tensor, feasibility_weight: float
+        polygon_to_check: torch.Tensor,
+        generated_lir: torch.Tensor,
+        feasibility_weight: float,
+        with_underfitting: bool = False,
     ) -> torch.Tensor:
         """compute the feasibility loss that checks the generated rectangle is within the input polygon
 
@@ -124,16 +125,15 @@ class LirGeometricLoss(nn.Module):
 
         generated_lir = (generated_lir > 0.5).float()
 
-        overextension_mask = (generated_lir == 1) & (input_polygon != 1)
+        feasibility_loss = ((generated_lir == 1) & (polygon_to_check == 0)).sum().float()
+        normalized_feasibility_loss = (feasibility_loss / polygon_to_check.sum().float()) ** 2
 
-        underfitting_mask = (generated_lir != 1) & (input_polygon == 1)
+        normalized_underfitting = torch.zeros_like(normalized_feasibility_loss)
+        if with_underfitting:
+            underfitting = ((generated_lir == 0) & (polygon_to_check == 1)).sum().float()
+            normalized_underfitting = (underfitting / polygon_to_check.sum().float()) ** 2
 
-        overextension_loss = overextension_mask.sum().float() / input_polygon.sum().float()
-        underfitting_loss = underfitting_mask.sum().float() / input_polygon.sum().float()
-
-        feasibility_loss = (overextension_loss + underfitting_loss) * feasibility_weight
-
-        return feasibility_loss
+        return (normalized_feasibility_loss + normalized_underfitting) * feasibility_weight
 
     @staticmethod
     def compute_connectivity_loss(generated_lir: torch.Tensor, connectivity_weight: float) -> torch.Tensor:
@@ -149,37 +149,13 @@ class LirGeometricLoss(nn.Module):
 
         generated_lir_np = (generated_lir.detach().cpu().numpy() > 0.5).astype("uint8").squeeze()
 
-        num_labels_gen, _ = cv2.connectedComponents(generated_lir_np)
+        num_labels_gen, _ = cv2.connectedComponents(generated_lir_np, connectivity=4)
         num_labels_gen -= 1
 
         connectivity_loss = torch.tensor(abs(num_labels_gen - 1)).float().to(generated_lir.device)
+        normalized_connectivity_loss = (connectivity_loss / generated_lir_np.size) ** 2
 
-        return connectivity_loss * connectivity_weight
-
-    @staticmethod
-    def compute_rectangular_shape_loss(generated_lir: torch.Tensor, rectangular_shape_weight: float) -> torch.Tensor:
-        """_summary_
-
-        Args:
-            generated_lir (torch.Tensor): _description_
-            rectangular_shape_weight (float): _description_
-
-        Returns:
-            torch.Tensor: _description_
-        """
-
-        generated_lir = (generated_lir > 0.5).float().squeeze()
-        generated_lir_vertices = torch.nonzero(generated_lir).detach().cpu().numpy()
-        generated_lir_convex_hull = cv2.convexHull(generated_lir_vertices).reshape(-1, 2)
-
-        inner_angles = utils.calculate_polygon_angles(generated_lir_convex_hull)
-
-        inner_angles_loss = abs(sum(inner_angles) - 360)
-        vertices_count_loss = abs(len(inner_angles) - 4)
-
-        rectangular_shape_loss = torch.tensor(inner_angles_loss + vertices_count_loss).float().to(generated_lir.device)
-
-        return rectangular_shape_loss * rectangular_shape_weight
+        return normalized_connectivity_loss * connectivity_weight
 
     def forward(
         self, input_polygons: torch.Tensor, generated_lirs: torch.Tensor, target_lirs: torch.Tensor
@@ -201,22 +177,22 @@ class LirGeometricLoss(nn.Module):
 
             diou_loss = LirGeometricLoss.compute_diou_loss(generated_lir, target_lir, self.diou_weight)
 
-            feasibility_loss = LirGeometricLoss.compute_feasibility_loss(
+            connectivity_loss = LirGeometricLoss.compute_connectivity_loss(generated_lir, self.connectivity_weight)
+
+            feasibility_loss_1 = LirGeometricLoss.compute_feasibility_loss(
                 input_polygon, generated_lir, self.feasibility_weight
             )
 
-            connectivity_loss = LirGeometricLoss.compute_connectivity_loss(generated_lir, self.connectivity_weight)
-
-            rectangular_shape_loss = LirGeometricLoss.compute_rectangular_shape_loss(
-                generated_lir, self.rectangular_shape_weight
+            feasibility_loss_2 = LirGeometricLoss.compute_feasibility_loss(
+                target_lir, generated_lir, self.feasibility_weight, with_underfitting=True
             )
 
             assert not torch.isnan(diou_loss).any(), "diou_loss is `nan`"
-            assert not torch.isnan(feasibility_loss).any(), "feasibility_loss is `nan`"
+            assert not torch.isnan(feasibility_loss_1).any(), "feasibility_loss_1 is `nan`"
+            assert not torch.isnan(feasibility_loss_2).any(), "feasibility_loss_2 is `nan`"
             assert not torch.isnan(connectivity_loss).any(), "connectivity_loss is `nan`"
-            assert not torch.isnan(rectangular_shape_loss).any(), "rectangular_shape_loss is `nan`"
 
-            loss = bce_loss + diou_loss + feasibility_loss + connectivity_loss
+            loss = bce_loss + diou_loss + connectivity_loss + feasibility_loss_1 + feasibility_loss_2
 
             total_loss += loss
 
