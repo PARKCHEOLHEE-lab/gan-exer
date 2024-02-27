165ab7c53afe33c6e1db95d37560bc574865d286
PARKCHEOLHEE-lab <qhqhahah@naver.com>
2024-02-27 20:28:29 +0900

    Add switch for `use_ray`

--- lirGAN/data/largest_inscribed_rectangle.py
+++ lirGAN/data/largest_inscribed_rectangle.py
@@ -1,5 +1,6 @@
 import ray
 import numpy as np
+import multiprocessing
 
 from typing import Tuple
 from shapely.geometry import Polygon
@@ -53,7 +54,37 @@ class LargestInscribedRectangle:
 
     @staticmethod
     @ray.remote
-    def _get_each_lir(
+    def _get_each_lir_with_ray(
+        rotation_degree: float, rotation_anchor: np.ndarray, binary_grid_shaped_polygon: np.ndarray
+    ) -> Polygon:
+        """Get the largest inscribed rectangle aligned by global xy axes on a given polygon
+           and convert it to a vectorized polygon
+
+        Args:
+            rotation_degree (float): degree to rotate
+            rotation_anchor (np.ndarray): anchor to rotate
+            binary_grid_shaped_polygon (np.ndarray): 2d binary grid-shaped polygon consisting of 0s, 1s
+
+        Returns:
+            Polygon: the largest rectangle aligned by global xy axes on a given polygon
+        """
+
+        top_left, bottom_right = LargestInscribedRectangle._get_lir_indices(binary_grid_shaped_polygon)
+
+        top_left_row, top_left_col = top_left
+        bottom_right_row, bottom_right_col = bottom_right
+
+        lir = np.zeros_like(binary_grid_shaped_polygon)
+        lir[top_left_row : bottom_right_row + 1, top_left_col : bottom_right_col + 1] = 1
+
+        lir_polygon = Polygon(utils.vectorize_polygon_from_array(lir))
+
+        inverted_lir_polygon = affinity.rotate(geom=lir_polygon, angle=-rotation_degree, origin=rotation_anchor)
+
+        return inverted_lir_polygon
+
+    @staticmethod
+    def _get_each_lir_without_ray(
         rotation_degree: float, rotation_anchor: np.ndarray, binary_grid_shaped_polygon: np.ndarray
     ) -> Polygon:
         """Get the largest inscribed rectangle aligned by global xy axes on a given polygon
@@ -87,6 +118,8 @@ class LargestInscribedRectangle:
         self,
         coordinates: np.ndarray,
         canvas_size: np.ndarray,
+        lir_rotation_degree_interval: float,
+        use_ray: bool = True,
     ) -> Polygon:
         """Find the largest inscribed rectangle on a given polygon(coordinates)
 
@@ -125,9 +158,16 @@ class LargestInscribedRectangle:
                 )
             )
 
-            rotation_degree += self.lir_rotation_degree_interval
+            rotation_degree += lir_rotation_degree_interval
 
-        lir_futures = [LargestInscribedRectangle._get_each_lir.remote(*args) for args in lir_args]
-        lirs = ray.get(lir_futures)
+        if use_ray:
+            if not ray.is_initialized():
+                ray.init(num_cpus=multiprocessing.cpu_count())
+
+            lir_futures = [LargestInscribedRectangle._get_each_lir_with_ray.remote(*args) for args in lir_args]
+            lirs = ray.get(lir_futures)
+
+        else:
+            lirs = [LargestInscribedRectangle._get_each_lir_without_ray(*args) for args in lir_args]
 
         return max(lirs, key=lambda p: p.area)
