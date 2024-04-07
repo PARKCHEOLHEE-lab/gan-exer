cb8b5603089b41ec570d88286bbe66a182c5d138
PARKCHEOLHEE-lab <qhqhahah@naver.com>
2024-04-07 18:26:42 +0900

    Update `create_animation_gif` of commonutils.py

--- commonutils.py
+++ commonutils.py
@@ -11,6 +11,8 @@ def create_animation_gif(
     format: str = "png",
     loop: int = 0,
     duration: int = 1,
+    sorting: bool = True,
+    add_reverse_frames: bool = False,
 ) -> None:
     """Create an animated GIF from a directory of images.
 
@@ -20,10 +22,18 @@ def create_animation_gif(
         format (str, optional): Format of the image files (default is "png").
         loop (int, optional): Number of loops for the GIF (0 for infinite looping, default is 0).
         duration (int, optional): Duration (in milliseconds) of each frame (default is 1).
+        sorting (bool, optional): Whether to sort the images by their names (default is False).
+        add_reverse_frames (bool, optional): Whether to add reverse frames (default is False).
     """
 
-    files = sorted(os.listdir(images_directory), key=lambda x: int(x.split("-")[-1].split(".")[0]))
+    files = os.listdir(images_directory)
+    if sorting:
+        files = sorted(files, key=lambda x: int(x.split("-")[-1].split(".")[0]))
+
     files = [os.path.abspath(os.path.join(images_directory, file)) for file in files if file.endswith(format)]
+
+    if add_reverse_frames:
+        files += files[::-1]
 
     images_data = []
     for file in files:
